f77da956716e7f8a8a88520008ed9aa2137f19e8
Затолокин Павел <ZatolokinPavel@yandex.ru>
2018-04-23 21:35:37 +0300

    split index.php with header.php and footer.php

---
+++ wordpress/header.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Шаблон шапки (header.php)
+ */
+?><!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Живое Слово</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+    <?php wp_head(); ?>
+</head>
+<body id="body" class="noscript">
+
+<!-- Для браузеров, где не работает js, применяем дополнительные стили -->
+<script>document.getElementById('body').classList.remove("noscript")</script>
+
+<header class="main-header">
+    <div class="container">
+        <div class="rhombuses box">
+            <div class="rhombus r1">
+                <div class="kharin">Александр Харин<br>руководитель проекта</div>
+            </div>
+            <div class="rhombus r2"></div>
+            <div class="rhombus r3"></div>
+            <div class="rhombus r4">
+                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile1.jpg')"></p>
+            </div>
+            <div class="rhombus big">
+                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile_big.jpg')"></p>
+            </div>
+            <div class="rhombus r5">
+                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile4.jpg')"></p>
+            </div>
+            <div class="rhombus r6">
+                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile3.jpg')"></p>
+            </div>
+            <div class="rhombus r7">
+                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile5.jpg')"></p>
+            </div>
+            <div class="rhombus r8"></div>
+            <div class="rhombus r9"></div>
+            <div class="rhombus r10"></div>
+            <div class="rhombus r11"></div>
+            <div class="rhombus r12"></div>
+        </div>
+    </div>
+</header>
+
+<menu id="main_menu" class="main-menu box">
+    <div>
+        <ul class="top">
+            <li><a href="/">Главная</a></li>
+            <li><a href="#">Тренинги</a></li>
+            <li><a href="/blog-o-psyhologii.html">Статьи</a></li>
+            <li><a href="/konsultatsii-psihologa.html">Консультации</a></li>
+            <li><a href="#">Новости</a></li>
+            <li><a href="/raspisanie">Расписание</a></li>
+            <li><a href="/oplata-i-skidki.html">Стоимость</a></li>
+            <li><a href="/video">Видео</a></li>
+            <li><a href="/kontakty.html">Контакты</a></li>
+            <li><a href="/o-nas.html">О&nbsp;нас</a></li>
+        </ul>
+    </div>
+</menu>
--- wordpress/index.php
+++ wordpress/index.php
@@ -1,69 +1,10 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Живое Слово</title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
-    <?php wp_head(); ?>
-</head>
-<body id="body" class="noscript">
+<?php
+/**
+ * Главная страница (index.php)
+ */
+?>
 
-<!-- Для браузеров, где не работает js, применяем дополнительные стили -->
-<script>document.getElementById('body').classList.remove("noscript")</script>
-
-<header class="main-header">
-    <div class="container">
-        <div class="rhombuses box">
-            <div class="rhombus r1">
-                <div class="kharin">Александр Харин<br>руководитель проекта</div>
-            </div>
-            <div class="rhombus r2"></div>
-            <div class="rhombus r3"></div>
-            <div class="rhombus r4">
-                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile1.jpg')"></p>
-            </div>
-            <div class="rhombus big">
-                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile_big.jpg')"></p>
-            </div>
-            <div class="rhombus r5">
-                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile4.jpg')"></p>
-            </div>
-            <div class="rhombus r6">
-                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile3.jpg')"></p>
-            </div>
-            <div class="rhombus r7">
-                <p class="tile" style="background-image: url('<?php bloginfo('template_url'); ?>/images/tile5.jpg')"></p>
-            </div>
-            <div class="rhombus r8"></div>
-            <div class="rhombus r9"></div>
-            <div class="rhombus r10"></div>
-            <div class="rhombus r11"></div>
-            <div class="rhombus r12"></div>
-        </div>
-    </div>
-</header>
-
-<menu id="main_menu" class="main-menu box">
-    <div>
-        <ul class="top">
-            <li><a href="/">Главная</a></li>
-            <li><a href="#">Тренинги</a></li>
-            <li><a href="/blog-o-psyhologii.html">Статьи</a></li>
-            <li><a href="/konsultatsii-psihologa.html">Консультации</a></li>
-            <li><a href="#">Новости</a></li>
-            <li><a href="/raspisanie">Расписание</a></li>
-            <li><a href="/oplata-i-skidki.html">Стоимость</a></li>
-            <li><a href="/video">Видео</a></li>
-            <li><a href="/kontakty.html">Контакты</a></li>
-            <li><a href="/o-nas.html">О&nbsp;нас</a></li>
-        </ul>
-    </div>
-</menu>
-
-
-
+<?php get_header(); ?>
 <section class="slogan container box">
     <div class="grid-line">
         <div class="col4">
@@ -199,39 +140,4 @@
         </div>
     </div>
 </div>
-
-
-
-<footer>
-    <div class="footer-top container">
-        logo
-        <div class="counters">
-            <div><p>12</p>лет<br>на рынке</div>
-            <div><p>1768</p>довольных<br>клиентов</div>
-            <div><p>73</p>тренинга<br>проведено</div>
-        </div>
-    </div>
-    <div class="footer-bottom container box">
-        <div class="copyright">
-            <p>
-                Использование материалов, размещённых на сайте, разрешается только
-                при наличии активной ссылки на данный ресурс.
-            </p>
-        </div>
-        <div class="contacts" itemscope itemtype="http://schema.org/Organization">
-            <p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                г. <span itemprop="addressLocality">Днепр</span>,
-                <span itemprop="streetAddress">ул. Воскресенская, 27/35</span><br>
-                (бывш. ул. Ленина)
-            </p>
-            <p><a href="mailto:info@zhivoeslovo.com" itemprop="email">info@zhivoeslovo.com</a></p>
-            <p>
-                <span itemprop="telephone">+380 (97) 33-44-505</span><br>
-                <span itemprop="telephone">+380 (66) 33-44-509</span>
-            </p>
-        </div>
-    </div>
-</footer>
-
-</body>
-</html>
+<?php get_footer(); ?>
