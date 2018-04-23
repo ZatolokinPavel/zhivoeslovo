4be20517cae88f6be066c2a05637ffb9e8c6ffeb
Затолокин Павел <ZatolokinPavel@yandex.ru>
2018-04-23 23:12:33 +0300

    add simple wp-loop to index.php

--- wordpress/index.php
+++ wordpress/index.php
@@ -9,6 +9,13 @@
 
 <?php get_header(); ?>
 <section class="container box">
-
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+            the_content();
+        endwhile;
+    endif;
+    ?>
 </section>
 <?php get_footer(); ?>
